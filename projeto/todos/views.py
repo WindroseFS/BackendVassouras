fb6ba982ac667e601a27f5513d78d9bbcb7448f0
WindroseFS <almir.junior445@gmail.com>
2025-03-10 13:20:31 -0300

    2024/03/10 commit 2

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, '/projeto/todos/templates/todos/home.html');
+    return render(request, 'todos/home.html');
