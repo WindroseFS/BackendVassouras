7890a3694c5a7083d3b3aeeb1392bd5ff9f02a03
Jaqueline <jaquedielle@gmail.com>
2025-04-17 18:49:46 -0300

    2025/04/17 commit

--- projeto/setup/urls.py
+++ projeto/setup/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
-from todos.views import todo_list
+from todos.views import TodoListView, TodoCreateView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", todo_list),
+    path('', TodoListView.as_view(), name="todo_list"),
+    path('create', TodoCreateView.as_view(), name="todo_create")
 ]
--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
 from .models import Todo
-# from django.http import HttpResponse
+from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
+
+class TodoListView(ListView):
+    model = Todo
 
 
-# Create your views here.
-def todo_list_old(request):
-    nome = 'Rodrigo'
-    alunos = ['1. Ana', '2. José', '3. Bia']
-    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
-
-
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
