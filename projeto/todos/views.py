026568eab85136881d09a785fce2a303b63185b2
WindroseFS <almir.junior445@gmail.com>
2025-05-15 20:48:36 -0300

    2025/05/15 commit

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,5 +1,5 @@
 from .models import Todo
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 class TodoListView(ListView):
@@ -9,4 +9,13 @@ class TodoListView(ListView):
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = Todo
     success_url = reverse_lazy("todo_list")
